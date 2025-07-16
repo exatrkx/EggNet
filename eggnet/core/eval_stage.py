b694f1a04a950b764f037541cd3303d7be9528e2
ly49nkallo <tyabrennan@gmail.com>
2025-07-16 15:36:03 -0700

    update eval + plotting

--- eggnet/core/eval_stage.py
+++ eggnet/core/eval_stage.py
@@ -5,16 +5,17 @@ import pandas as pd
 
 from eggnet import lightning_modules
 from eggnet.utils.cluster import cluster_and_match
-from eggnet.utils.plotting import plot_eff_vs_eps, plot_eff_fixed_eps, plot_computing_time
+from eggnet.utils.plotting import plot_eff_vs_eps, plot_eff_fixed_eps, plot_computing_time, plot_hit_parameter_prediction_accuracy
 from eggnet.utils.slurm import submit_to_slurm
 
 
 def eval(config_file, eval_config_file, output_dir, accelerator, dataset, slurm):
-
+    # plot_eff_vs_eps
+    # plot_eff_fixed_eps
+    # plot_computing_time
     if slurm:
         eval_slurm(config_file, eval_config_file, output_dir, accelerator, dataset)
         return
-
     with open(config_file, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     if output_dir is not None:
@@ -28,23 +29,23 @@ def eval(config_file, eval_config_file, output_dir, accelerator, dataset, slurm)
     base_model.datasets = dataset
     base_model.setup(stage="test")
     data = getattr(base_model, dataset)
-
-    eps_data = pd.DataFrame({
-        "eps": np.arange(0.05, 0.51, 0.05),
-        "n_particles": 0,
-        "n_matched_particles": 0,
-        "n_matched_tracks": 0,
-        "n_matched_target_particles": 0,
-        "n_matched_target_tracks": 0,
-        "n_tracks": 0,
-    })
-
-    time_data = pd.DataFrame({
-        "num_nodes": [],
-        "eggnet": [],
-        "knn": [],
-        "dbscan": [],
-    })
+    if any([eval_config.get(key, False) for key in eval_config.keys() if 'eps' in key]):
+        eps_data = pd.DataFrame({
+            "eps": np.arange(0.05, 0.51, 0.05),
+            "n_particles": 0,
+            "n_matched_particles": 0,
+            "n_matched_tracks": 0,
+            "n_matched_target_particles": 0,
+            "n_matched_target_tracks": 0,
+            "n_tracks": 0,
+        })
+    if eval_config.get("plot_computing_time", False):
+        time_data = pd.DataFrame({
+            "num_nodes": [],
+            "eggnet": [],
+            "knn": [],
+            "dbscan": [],
+        })
 
     if eval_config.get("pT_unit", "MeV") == "MeV":
         pt_min, pt_max = 1000, 50000
@@ -61,41 +62,50 @@ def eval(config_file, eval_config_file, output_dir, accelerator, dataset, slurm)
 
     for event in tqdm(data):
         event = event.to(accelerator)
+        
+        if any([eval_config.get(key, False) for key in eval_config.keys() if 'eps' in key]):
+            for eps_i in eps_data.eps:
+                eps_data_i, particles_pt_hist_i, matched_target_particles_pt_hist_i, particles_eta_hist_i, matched_target_particles_eta_hist_i = cluster_and_match(event, eps_i, eval_config, time_yes=True if eps_i == eval_config["eps"] else False)
 
-        for eps_i in eps_data.eps:
-            eps_data_i, particles_pt_hist_i, matched_target_particles_pt_hist_i, particles_eta_hist_i, matched_target_particles_eta_hist_i = cluster_and_match(event, eps_i, eval_config, time_yes=True if eps_i == eval_config["eps"] else False)
+                eps_data[eps_data.eps == eps_i] = eps_data[eps_data.eps == eps_i].to_numpy() + eps_data_i.to_numpy()
 
-            eps_data[eps_data.eps == eps_i] = eps_data[eps_data.eps == eps_i].to_numpy() + eps_data_i.to_numpy()
+                if eps_i == eval_config["eps"]:
+                    particles_pt_hist += particles_pt_hist_i
+                    matched_target_particles_pt_hist += matched_target_particles_pt_hist_i
+                    if eval_config.get("plot_eta", True):
+                        particles_eta_hist += particles_eta_hist_i
+                        matched_target_particles_eta_hist += matched_target_particles_eta_hist_i
 
-            if eps_i == eval_config["eps"]:
-                particles_pt_hist += particles_pt_hist_i
-                matched_target_particles_pt_hist += matched_target_particles_pt_hist_i
-                if eval_config.get("plot_eta", True):
-                    particles_eta_hist += particles_eta_hist_i
-                    matched_target_particles_eta_hist += matched_target_particles_eta_hist_i
-
-        time_data = pd.concat([time_data, pd.DataFrame({
-            "num_nodes": [event["num_nodes"].cpu()],
-            "eggnet": [event["BaseModule.forward"]],
-            "knn": [event[f"{config.get('knn_algorithm', 'cu_knn')}.get_graph"]],
-            "dbscan": [event["cluster"]],
-        })])
+        if eval_config.get("plot_computing_time", False):
+            time_data = pd.concat([time_data, pd.DataFrame({
+                "num_nodes": [event["num_nodes"].cpu()],
+                "eggnet": [event["BaseModule.forward"]],
+                "knn": [event[f"{config.get('knn_algorithm', 'cu_knn')}.get_graph"]],
+                "dbscan": [event["cluster"]]
+                }
+        )])
 
     # check metric!!
-    eps_data["eff"] = eps_data.n_matched_target_particles / eps_data.n_particles
-    eps_data["dup"] = (
-        eps_data.n_matched_target_tracks - eps_data.n_matched_target_particles
-    ) / eps_data.n_matched_target_particles
-    eps_data["fak"] = (eps_data.n_tracks - eps_data.n_matched_tracks) / eps_data.n_matched_particles
+    if any([eval_config.get(key, False) for key in eval_config.keys() if 'eps' in key]):
+        eps_data["eff"] = eps_data.n_matched_target_particles / eps_data.n_particles
+        eps_data["dup"] = (
+            eps_data.n_matched_target_tracks - eps_data.n_matched_target_particles
+        ) / eps_data.n_matched_target_particles
+        eps_data["fak"] = (eps_data.n_tracks - eps_data.n_matched_tracks) / eps_data.n_matched_particles
 
-    time_data["gnn"] = time_data["eggnet"] - time_data["knn"]
-    time_data["total"] = time_data["eggnet"] + time_data["dbscan"]
-
-    plot_eff_vs_eps(eps_data, eval_config)
-    plot_eff_fixed_eps(matched_target_particles_pt_hist, particles_pt_hist, eps_data, eval_config, pt_bins, f"$p_T$ [{eval_config.get('pT_unit', 'MeV')}]", logx=True, filename="track_efficiency_pt.png")
-    if eval_config.get("plot_eta", True):
+    if eval_config.get("plot_computing_time", False):
+        time_data["gnn"] = time_data["eggnet"] - time_data["knn"]
+        time_data["total"] = time_data["eggnet"] + time_data["dbscan"]
+    if eval_config.get("plot_eff_vs_eps", False):
+        plot_eff_vs_eps(eps_data, eval_config)
+    if eval_config.get("plot_eff_fixed_eps", False):
+        plot_eff_fixed_eps(matched_target_particles_pt_hist, particles_pt_hist, eps_data, eval_config, pt_bins, f"$p_T$ [{eval_config.get('pT_unit', 'MeV')}]", logx=True, filename="track_efficiency_pt.png")
+    if eval_config.get("plot_eta", False):
         plot_eff_fixed_eps(matched_target_particles_eta_hist, particles_eta_hist, eps_data, eval_config, eta_bins, r"$\eta$", logx=False, filename="track_efficiency_eta.png")
-    plot_computing_time(time_data, eval_config)
+    if eval_config.get("plot_parameter_predict", False):
+        plot_hit_parameter_prediction_accuracy(data, config, eval_config)
+    if eval_config.get("plot_computing_time", False):
+        plot_computing_time(time_data, eval_config)
 
 
 def eval_slurm(config_file, eval_config_file, output_dir, accelerator, dataset):
